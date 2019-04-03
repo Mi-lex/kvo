5391da34e83dd127c02fbb10a06f579e6325f31b
= <=>
2019-04-03 10:18:56 +0300

    Make links active depending on the page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,11 +1,35 @@
+<?php
+    $categories = [
+        '/' => [
+            'name' => 'Главная',
+        ],
+        'news' => [
+            'name' => 'Новости',
+        ],
+        'documents' => [
+            'name' => 'Документы',
+        ],
+        'gallary' => [
+            'name' => 'Фотоальбомы',
+        ],
+        'contacts' => [
+            'name' => 'Контакты',
+        ],
+        'vacation' => [
+            'name' => 'Базы отдыха',
+        ],
+        'huntinginfo' => [
+            'name' => 'Охотминимум',
+        ],
+    ];
+?>
+
 <nav id="nav">
     <ul class="links">
-        <li class="active"><a href="{{ url('/') }}">Главная</a></li>
-        <li><a href="#">Новости</a></li>
-        <li><a href="#">Документы</a></li>
-        <li><a href="#">Фотоальбомы</a></li>
-        <li><a href="#">Контакты</a></li>
-        <li><a href="#">Базы отдыха</a></li>
-        <li><a href="#">Охотминимум</a></li>
+        @foreach ($categories as $link => $category)
+            <li class="{{ Request::is($link) ? 'active' : null }}">
+                <a href="{{ url($link) }}">{{ $category['name'] }}</a>
+            </li> 
+        @endforeach
     </ul>
 </nav>
