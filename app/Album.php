02074382088358387762035e5a8ba469efef05bb
Mishin Alexey <mishin-lex@mail.ru>
2019-04-09 12:01:35 +0300

    Add migration and model for albums

---
+++ app/Album.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Album extends Model
+{
+    protected $fillable = ['title', 'description'];
+
+    public function images()
+    {
+        return $this->morphMany('App\Image', 'imagable');
+    }
+
+    public function main_image()
+    {
+        return $this->images()->first()->filename;    
+    }
+}
